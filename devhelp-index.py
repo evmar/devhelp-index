a95231b0d64e81a26949d66772ff6d75ce2da8c6
Evan Martin <martine@danga.com>
2011-09-28 12:15:35 -0700

    bzip

--- devhelp-index.py
+++ devhelp-index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import bz2
 import glob
 import logging
 import os
@@ -7,7 +8,7 @@ import xml.etree.ElementTree as ET
 import sys
 
 CACHE_PATH = os.environ.get('XDG_CACHE_HOME', os.path.expanduser('~/.cache'))
-DB_PATH = os.path.join(CACHE_PATH, 'devhelp-index.db')
+DB_PATH = os.path.join(CACHE_PATH, 'devhelp-index.bz2')
 GTK_DOC_PATH = '/usr/share/gtk-doc/html/'
 
 def devhelp_tag(name):
@@ -35,27 +36,38 @@ def process(path):
             if len(words) != 1:
                 logging.warning('unhandled: ' + str(attrs))
                 continue
+            word = words[0]
+
+            if word.endswith('()'):
+                word = word[:-2]
             # XXX check word is only ascii
-            yield words[0], attrs['link']
+            yield word, attrs['link']
         elif attrs['type'] in ('', 'property', 'signal'):
             pass
         else:
             raise NotImplementedError, attrs
 
 def build_index(verbose=True):
-    with open(DB_PATH, 'w') as f:
-        for path in glob.glob(GTK_DOC_PATH + '*/*.devhelp2'):
-            if verbose:
-                print >>sys.stderr, '*', path
-            dir = os.path.dirname(path[len(GTK_DOC_PATH):])
-            for kw, link in process(path):
-                link = os.path.join(dir, link)
-                print >>f, '%s %s' % (kw, link)
+    keyvals = []
+
+    for path in glob.glob(GTK_DOC_PATH + '*/*.devhelp2'):
+        if verbose:
+            print >>sys.stderr, '*', path
+        dir = os.path.dirname(path[len(GTK_DOC_PATH):])
+        for kw, link in process(path):
+            link = os.path.join(dir, link)
+            keyvals.append((kw, link))
+
+    keyvals.sort()
+
+    with bz2.BZ2File(DB_PATH, 'w') as f:
+        for kw, link in keyvals:
+            print >>f, '%s %s' % (kw, link)
 
 def get_or_rebuild_index(verbose=True):
     if not os.path.exists(DB_PATH):
         build_index(verbose)
-    return open(DB_PATH, 'r')
+    return bz2.BZ2File(DB_PATH, 'r')
 
 if __name__ == '__main__':
     build_index()
